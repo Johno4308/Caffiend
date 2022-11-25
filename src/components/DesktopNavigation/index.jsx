dfafb48ad4dd7f0d7e27cc3de9d9a5822786af13
Johno4308 <67054611+Johno4308@users.noreply.github.com>
2022-11-25 19:21:08 +0200

    Sidebar progress

--- src/components/DesktopNavigation/index.jsx
+++ src/components/DesktopNavigation/index.jsx
@@ -1,15 +1,50 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './index.scss'
 import Logo from '../../assets/Images/logo-brown-trans.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faPhone, faHandshake } from '@fortawesome/free-solid-svg-icons'
 
 const DesktopNavigation = () => {
     return(
-        <nav className='nav-bar'>
+        <div className='nav-bar'>
             <Link className='logo' to='/'>
                 <img src={Logo} alt='logo'/>
                 <p alt='logo-text'>Caffiend</p>
             </Link>
-        </nav>
+            <nav>
+                <NavLink exact="true" activeclassname="active" to="/">
+                    <div>
+                    <section>
+                    <hr />
+                    <FontAwesomeIcon icon={faCoffee} color="#684028" />
+                    <p>Coffees</p> 
+                    <hr />
+                    </section>
+                    </div>
+                </NavLink>
+                <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                    <div>
+                    <section>
+                    <hr />
+                    <FontAwesomeIcon icon={faPhone} color="#684028" />
+                    <p>Contact Us</p> {/*fa-phone*/}
+                    <hr />
+                    </section>
+                    </div>
+                </NavLink>
+                <NavLink exact="true" activeclassname="active" className="acknowledge-link"to="/acknowledge">
+                    <div>
+                    <section>
+                    <hr />
+                    <FontAwesomeIcon icon={faHandshake} color="#684028" />
+                    <p>Acknowledge</p> {/*fa-handshake-o*/}
+                    <hr />
+                    </section>
+                    </div>
+                </NavLink>
+            </nav>
+        </div>
+
     );
 }
 
