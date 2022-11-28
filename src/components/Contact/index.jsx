4ca69ff73b6f68fa7a944c2e0b05e26902b3e93a
Johno4308 <67054611+Johno4308@users.noreply.github.com>
2022-11-28 19:36:29 +0200

    Some styling on contact page

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,61 +1,55 @@
 import './index.scss'
 
+import { Suspense, useRef } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { OrbitControls, useGLTF } from '@react-three/drei'
+
+function CoffeeModel(props) {
+    const group = useRef()
+    const { nodes, materials } = useGLTF("/Coffee3D.glb");
+    useFrame( ({clock}) => {
+        group.current.rotation.y = clock.getElapsedTime() /3
+    })
+    return (
+      <group ref={group} {...props} dispose={null}>
+        <group rotation={[-Math.PI / 2, 0, 0]} scale={3}>
+          <group rotation={[Math.PI / 2, 0, 0]}>
+            <mesh
+              castShadow
+              receiveShadow
+              geometry={nodes.defaultMaterial.geometry}
+              material={materials.DefaultMaterial}
+            />
+          </group>
+        </group>
+      </group>
+    );
+  }
+
+
+
+//add contact tags and finish off the acknowledge page and styling
+
 const Contact = () => {
-
-
-
     return(
-        <>
         <div className='contact-textzone'>
             <h1>Contact Me</h1>
             <h2>I would love you hear from you</h2>
             <p>Whether its a compliment, a critque or you burnt your hand touching that espresso while using this site</p>
             <p>Or maybe you are looking for a Junior Web Developer, whatever the case, i'm all ears.</p>
+            
+            <div className='model-wrapper'>
+                <Canvas camera={{ fov:30, position: [0,15,5]}}>
+                    <Suspense fallback={null}>
+                    <ambientLight />
+                    <directionalLight intensity={2} positon={[0,0,50]} />
+                    <CoffeeModel />
+                    <OrbitControls enablePan={true} enableZoom={true} enableRotate={true} />
+                </Suspense>
+                </Canvas>
+            </div>
         </div>
-        <div className='contact-form'>
-        {/* <form ref={refForm} onSubmit={sendEmail}>    */}
-        <form>
-            <ul>
-                <li className="half">
-                <input 
-                    placeholder="Name" 
-                    type="text" 
-                    name="name" 
-                    required />
-                </li>
-                <li className="half">
-                <input
-                    placeholder="Email"
-                    type="email"
-                    name="email"
-                    required
-                />
-                </li>
-                <li>
-                <input
-                    placeholder="Subject"
-                    type="text"
-                    name="subject"
-                    required
-                />
-                </li>
-                <li>
-                <textarea
-                    placeholder="Message"
-                    name="message"
-                    required
-                ></textarea>
-                </li>
-                <li>
-                <input 
-                    type="submit" 
-                    className="flat-button" 
-                    value="SEND" />
-                </li>
-            </ul>
-        </form>
-    </div>
-    </>
+
     )
 }
 
