459f89fd817682860b01c7abcdeee265903865b3
Johno4308 <67054611+Johno4308@users.noreply.github.com>
2022-11-29 11:42:07 +0200

    Attempting Netlify Fix

--- src/components/Coffees/index.jsx
+++ src/components/Coffees/index.jsx
@@ -10,7 +10,7 @@ const Coffees = (props) => {
             <h1>{props.data.title}</h1>
             <p> {props.data.description}</p>
             <section>
-                <img src={`/src/Images/${props.data.img}`} />
+                <img src={`../src/Images/${props.data.img}`} />
                 <p>{props.data.ratio}</p>
                 <p>{props.data.cup}</p>
             </section>
